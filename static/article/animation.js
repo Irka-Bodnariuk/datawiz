3f484b25977354f2bcbb0523f45f80b52933e1aa
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-05-19 15:11:43 +0300

    change of animation

--- static/article/animation.js
+++ static/article/animation.js
@@ -1,20 +1,16 @@
+const columnCharts = [];
+
 const path1 = document.getElementById("path1");
 const path2 = document.getElementById("path2");
 
 const listItems = document.querySelectorAll("li.content_block");
 
-const columns1 = document
-  .getElementById("ColumnChart1")
-  .getElementsByTagName("path");
-const columns2 = document
-  .getElementById("ColumnChart2")
-  .getElementsByTagName("path");
-const columns3 = document
-  .getElementById("ColumnChart3")
-  .getElementsByTagName("path");
-const columns4 = document
-  .getElementById("ColumnChart4")
-  .getElementsByTagName("path");
+for (let i = 1; i <= 4; i++) {
+  const chart = document
+    .getElementById(`ColumnChart${i}`)
+    .getElementsByTagName("path");
+  columnCharts.push(chart);
+}
 
 const length1 = path1.getTotalLength();
 const length2 = path2.getTotalLength();
@@ -27,8 +23,8 @@ gsap.set([path1, path2], {
 const line = gsap.timeline();
 
 line
-  .to(path1, { duration: 4, strokeDashoffset: 0, ease: "linear" })
-  .to(path2, { duration: 4, strokeDashoffset: 0, ease: "linear" }, "-=4");
+  .to(path1, { duration: 4, strokeDashoffset: 0, ease: "power1.in" })
+  .to(path2, { duration: 4, strokeDashoffset: 0, ease: "power1.in" }, "-=4");
 
 for (let i = 1; i < listItems.length; i += 2) {
   const listItem = listItems[i];
@@ -47,54 +43,19 @@ for (let i = 1; i < listItems.length; i += 2) {
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(columns1, {
-  scaleY: 0,
-  transformOrigin: "bottom",
-  duration: 1,
-  ease: "power4.out",
-  stagger: 0.2,
-  delay: 0.2,
-  scrollTrigger: {
-    trigger: columns1,
-    toggleAction: "restart none none none",
-  },
-});
+for (let i = 0; i < columnCharts.length; i++) {
+  gsap.from(columnCharts[i], {
+    scaleY: 0,
+    transformOrigin: "bottom",
+    duration: 1,
+    ease: "power4.out",
+    stagger: 0.2,
+    delay: 0.2,
+    scrollTrigger: {
+      trigger: columnCharts[i],
+      toggleAction: "restart none none none",
+    },
+  });
+}
 
-gsap.from(columns2, {
-  scaleY: 0,
-  transformOrigin: "bottom",
-  duration: 1,
-  ease: "power4.out",
-  stagger: 0.2,
-  delay: 0.2,
-  scrollTrigger: {
-    trigger: columns2,
-    toggleAction: "restart none none none",
-  },
-});
-
-gsap.from(columns3, {
-  scaleY: 0,
-  transformOrigin: "bottom",
-  duration: 1,
-  ease: "power4.out",
-  stagger: 0.2,
-  delay: 0.2,
-  scrollTrigger: {
-    trigger: columns3,
-    toggleAction: "restart none none none",
-  },
-});
-
-gsap.from(columns4, {
-  scaleY: 0,
-  transformOrigin: "bottom",
-  duration: 1,
-  ease: "power4.out",
-  stagger: 0.2,
-  delay: 0.2,
-  scrollTrigger: {
-    trigger: columns4,
-    toggleAction: "restart none none none",
-  },
-});
+// change of animation
